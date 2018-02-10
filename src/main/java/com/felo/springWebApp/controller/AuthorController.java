5293949a2642e66558b4978f364ee7d08ca37e8e
felixmutua <felixmutua01@gmail.com>
2018-02-10 18:51:15 +0300

    Author Controller and view

---
+++ src/main/java/com/felo/springWebApp/controller/AuthorController.java
@@ -0,0 +1,24 @@
+package com.felo.springWebApp.controller;
+
+import com.felo.springWebApp.repository.AuthorRepository;
+import com.felo.springWebApp.repository.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository){
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthors(Model model){
+        model.addAttribute("authors", authorRepository.findAll());
+        return "authors";
+
+    }
+}
